8812069a55aed8885e47f2d3ef56f3590199b8cd
braffolk <estoniascape@gmail.com>
2023-12-07 05:38:10 +0200

    test for fixing env vars

--- packages/sprite-source/src/index.test.ts
+++ packages/sprite-source/src/index.test.ts
@@ -190,7 +190,7 @@ describe('Sprite Sources', function () {
   });
 
   it('can quickly update a sprite source', async function () {
-    const source = new SpriteSource(sandboxSource);
+    const source = new SpriteSource("./samples");
     const start = startTimer();
     await source.update({ ignore: ['--impl'] });
     endTimer(start, 'Updated sprite source');
--- packages/vscode/src/extension.ts
+++ packages/vscode/src/extension.ts
@@ -1,6 +1,12 @@
 import vscode from 'vscode';
 
 export async function activate(ctx: vscode.ExtensionContext): Promise<void> {
+  if (process.platform === 'darwin') {
+    process.env.APPDATA = process.env.APPDATA || `/Users/${process.env.USER}/.config`;
+    process.env.PROGRAMDATA = process.env.PROGRAMDATA || '/Users/Shared/';
+    process.env.PROGRAMFILES = process.env.PROGRAMFILES || `/Users/${process.env.USER}/Applications`;
+  }
+
   const imported = await import('./extension.workspace.mjs');
   await imported.StitchWorkspace.activate(ctx);
 }
